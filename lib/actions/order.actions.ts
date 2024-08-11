b3c3020fb951a16977ae11bb1991dbdf7a38a658
Abhishek Yadav <55094244+abhii145@users.noreply.github.com>
2024-08-11 19:28:48 +0530

    admin-area

--- lib/actions/order.actions.ts
+++ lib/actions/order.actions.ts
@@ -1,23 +1,29 @@
-// lib/actions/order.actions.ts
+"use server"
 
-import { CreateOrderParams, GetOrdersByEventParams } from "@/types"
+import {
+  CreateOrderParams,
+  GetOrdersByEventParams,
+  GetOrdersByUserParams,
+} from "@/types"
 import { redirect } from "next/navigation"
 import Stripe from "stripe"
 import Order from "../database/models/order.model"
 import { connectToDatabase } from "../database"
 import { ObjectId } from "mongodb"
+import Event from "../database/models/event.model"
+import User from "../database/models/user.model"
 
 export const checkoutOrder = async (order: any) => {
-     if (!process.env.NEXT_PUBLIC_STRIPE_SECRET_KEY) {
-       throw new Error("Stripe secret key is missing.")
-     }
+  if (!process.env.NEXT_PUBLIC_STRIPE_SECRET_KEY) {
+    throw new Error("Stripe secret key is missing.")
+  }
 
-    const stripe = new Stripe(
-      "sk_test_51PmGbZSHus4jNJZFFX4mYWaEkSkdPNKyzinPSi1PBf2X38ln0pivfPVdb4x8lxoEmbu1vNh7WigrBn67gh8vapH400M5GarHC2",
-      {
-        apiVersion: "2024-06-20",
-      }
-    )
+  const stripe = new Stripe(
+    "sk_test_51PmGbZSHus4jNJZFFX4mYWaEkSkdPNKyzinPSi1PBf2X38ln0pivfPVdb4x8lxoEmbu1vNh7WigrBn67gh8vapH400M5GarHC2",
+    {
+      apiVersion: "2024-06-20",
+    }
+  )
   const price = order.isFree ? 0 : Number(order.price) * 100
 
   try {
@@ -43,13 +49,12 @@ export const checkoutOrder = async (order: any) => {
       cancel_url: `${process.env.NEXT_PUBLIC_SERVER_URL}/`,
     })
 
-   return session.id
+    return session.id
   } catch (error) {
     console.error("Error creating checkout session:", error)
     throw error
   }
 }
-
 
 export const createOrder = async (order: CreateOrderParams) => {
   try {
@@ -64,6 +69,44 @@ export const createOrder = async (order: CreateOrderParams) => {
     return JSON.parse(JSON.stringify(newOrder))
   } catch (error) {
     console.log(error)
+    throw error
+  }
+}
+
+export async function getOrdersByUser(userId: string) {
+  try {
+    await connectToDatabase()
+    const orders = await Order.find({ buyer: userId }).exec()
+
+    const eventIds: string[] = []
+    orders.forEach((order) => {
+      const eventId = order.event.toString()
+      if (!eventIds.includes(eventId)) {
+        eventIds.push(eventId)
+      }
+    })
+
+    return {
+      data: JSON.parse(JSON.stringify(eventIds)),
+    }
+  } catch (error) {
+    console.log("Error fetching user orders:", error)
+    throw error
+  }
+}
+
+
+
+export async function getAllOrders() {
+  try {
+    await connectToDatabase()
+    const orders = await Order.find().exec()
+
+    return {
+      data: JSON.parse(JSON.stringify(orders)),
+    }
+  } catch (error) {
+    console.log("Error fetching all orders:", error)
     throw error
   }
 }
--- lib/database/index.ts
+++ lib/database/index.ts
@@ -1,6 +1,7 @@
 import mongoose from "mongoose"
 
-const MONGODB_URI = process.env.MONGODB_URI
+const MONGODB_URI =
+  "mongodb+srv://abhiy145:abhiy145@cluster0.m6mu3ii.mongodb.net/"
 
 let cached = (global as any).mongoose || { conn: null, promise: null }
 
